cdf53efcd82abee0db32b9884804d043c9df95e7
Sergey Pochikovskiy <sergey.pochikovskiy@gmail.com>
2026-09-08 22:02:10 -0700

    fix: resolve remaining Sentry build notices (instrumentation, global-error, ESM externals)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withSentryConfig } = require('@sentry/nextjs')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@sentry/server-utils', '@apm-js-collab/tracing-hooks'],
+  },
 }
 
 module.exports = withSentryConfig(nextConfig, {
